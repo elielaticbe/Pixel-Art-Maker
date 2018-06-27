1db9d07a74cc3103e70b8953e5e43bda4b7529e7
Elijah Laticbe <elielaticbe@gmail.com>
2018-06-27 02:27:21 +0100

    Initial commit

---
+++ designs.js
@@ -0,0 +1,29 @@
+let height, width, color;
+// Select color input
+$('#pixelCanvas').on('click','td', function(event){
+	color = $('#colorPicker').val();
+	$(event.target).css('background-color', color);
+});
+
+// Select size input
+$("#sizePicker").submit(function(event){
+	event.preventDefault();
+	height = $("#inputHeight").val();
+	width = $("#inputWeight").val();
+	makeGrid(height,width);
+	//console.log(height+"and"+width);
+});
+
+// When size is submitted by the user, call makeGrid()
+
+function makeGrid(h,w) {
+
+// Your code goes here!
+for(let h = 1; h<=height; h++){
+	$('#pixelCanvas').append('<tr></tr>');
+}
+for(let w = 1; w<=width; w++){
+	$('tr').append('<td></td>');
+}
+
+}
